915f327794dc10432039d82a4ebfdf6f31d2a67a
MrMocchy <patronummocchy@tiara.ocn.n.jp>
2022-07-02 23:25:59 +0900

    update
    swap,delete

--- MenuList/flutter_service_worker.js
+++ MenuList/flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7b2a98cada0bb9e2c23119f4cddc38d5",
-"/": "7b2a98cada0bb9e2c23119f4cddc38d5",
-"main.dart.js": "1e548de5e6fefd917f814745a7d808d3",
+"index.html": "08ab4a11d64281ddcc997a444567be42",
+"/": "08ab4a11d64281ddcc997a444567be42",
+"main.dart.js": "0fa0560d91e194d2b3163d201bbb767b",
 "manifest.json": "ed1c601775f95814287077b2fee0f99b",
 "version.json": "7a8f7d719cd69ebf2b1041da22fe3cd1"
 };
